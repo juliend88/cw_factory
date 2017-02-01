98e4a39304ff19ef4f55936a545df375f21d3a01
Maxence Maireaux <maxence@realstorm.fr>
2017-02-01 17:13:00 +0100

    Fix

--- v2/01_glance.sh
+++ v2/01_glance.sh
@@ -3,7 +3,9 @@
 TMP_IMG_NAME="$BASENAME-tmp-$BUILDMARK"
 IMG=ubuntu-14.04-server-cloudimg-amd64-disk1.img
 
+OS_USERNAME=$(OS_USERNAME)
+
 env
 echo $maxence
-
+echo OS_USERNAME
 openstack image create --disk-format qcow2 --container-format bare --file $IMG $TMP_IMG_NAME
--- v2/02_nova.sh
+++ v2/02_nova.sh
@@ -7,4 +7,4 @@ openstack server create   --flavor s1.cw.small-1 --image "0cd4d722-b54c-4b90-b43
 
 
 
-openstack server create   --flavor s1.cw.small-1 --image "6622878a-c762-4a88-b594-c30f255dcc06" --key-name jukey  --security-group b631b7d5-6212-4d2b-8c15-7a7d88d388f5 --nic net-id=505dae5a-7058-4fe3-a731-810e0a10357e --file /etc/cloud/cloud.cfg=cloud-init.yml youwin
+openstack server create   --flavor s1.cw.small-1 --image "6622878a-c762-4a88-b594-c30f255dcc06" --key-name jukey  --security-group default --nic net-id=32b1f969-3eed-48ff-a106-751ee10dc60b --file /etc/cloud/cloud.cfg=cloud-init.yml youwin
