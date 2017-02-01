fa5c6ff784f434df46af77928e7807401ca43c45
Maxence Maireaux <maxence@realstorm.fr>
2017-02-01 17:25:10 +0100

    Fix

--- v2/01_glance.sh
+++ v2/01_glance.sh
@@ -3,10 +3,4 @@
 TMP_IMG_NAME="$BASENAME-tmp-$BUILDMARK"
 IMG=ubuntu-14.04-server-cloudimg-amd64-disk1.img
 
-OS_USERNAME2=$OS_USERNAME
-OS_PASSWORD2=$OS_PASSWORD
-
-env
-echo $1
-echo $OS_USERNAME2
 openstack image create --disk-format qcow2 --container-format bare --file $IMG $TMP_IMG_NAME
