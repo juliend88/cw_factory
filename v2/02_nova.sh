7a22c0324e4233acf2ead95e05f1d370b2b6fad6
mohamed <lachhab.med.ali@gmail.com>
2017-02-01 19:58:24 +0100

    modif

--- v2/02_nova.sh
+++ v2/02_nova.sh
@@ -2,7 +2,9 @@
 
 TMP_IMG_ID="$(openstack image list --private | grep toto | tr "|" " " | tr -s " " | cut -d " " -f2)"
 
-openstack server create --flavor s1.cw.small-1 --image $TMP_IMG_ID  --key-name alikey  --security-group start-sg-start --nic net-id=7359a091-59a7-4e29-a068-ed2a64d8c068 --user-data cloud-init.yml ayoung-test1
+echo $TMP_IMG_ID
+openstack server create --flavor s1.cw.small-1 --image $TMP_IMG_ID --key-name alikey --security-group start-sg-start --nic net-id=7359a091-59a7-4e29-a068-ed2a64d8c068 --user-data cloud-init.yml ayoung-test1
 
+openstack image delete $TMP_IMG_ID
 
 #openstack server create  --flavor s1.cw.small-1 --image "6622878a-c762-4a88-b594-c30f255dcc06" --key-name jukey  --security-group default --nic net-id=32b1f969-3eed-48ff-a106-751ee10dc60b --file /etc/cloud/cloud.cfg=cloud-init.yml youwin
