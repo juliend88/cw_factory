20182177eca648360aa8323e32910b7ab1aa56a7
mohamed <lachhab.med.ali@gmail.com>
2017-02-01 19:46:40 +0100

    modif

--- v2/01_glance.sh
+++ v2/01_glance.sh
@@ -7,3 +7,8 @@ FILE=$(echo "${IMG_URL##*/}")
 
 openstack image create toto --disk-format qcow2 --container-format bare --file $FILE
 
+
+
+TMP_IMG_ID="$(openstack image list --private | grep toto | tr "|" " " | tr -s " " | cut -d " " -f2)"
+
+echo $TMP_IMG_ID
