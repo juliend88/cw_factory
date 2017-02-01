5c9576f21d22ad3701a84480be89036a8d6152c8
mohamed <lachhab.med.ali@gmail.com>
2017-02-01 19:12:55 +0100

    modif

--- v2/01_glance.sh
+++ v2/01_glance.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-wget http://cloud-images.ubuntu.com/releases/14.04/release/ubuntu-14.04-server-cloudimg-amd64-disk1.img
-openstack image create toto --disk-format qcow2 --container-format bare --file ubuntu-14.04-server-cloudimg-amd64-disk1.img
+wget $IMG_URL
+
+FILE=$(echo "${IMG_URL##*/})
+
+openstack image create toto --disk-format qcow2 --container-format bare --file $FILE
+
