d703c93244530f04f4ebdbf0895333271b081968
Maxence Maireaux <maxence@realstorm.fr>
2017-02-01 17:40:21 +0100

    Fix

--- v2/01_glance.sh
+++ v2/01_glance.sh
@@ -3,6 +3,6 @@
 TMP_IMG_NAME="$BASENAME-tmp-$BUILDMARK"
 IMG=ubuntu-14.04-server-cloudimg-amd64-disk1.img
 
-ls
+ls image-tmp-name
 
 openstack image create --disk-format qcow2 --container-format bare --file image-tmp-name toto
