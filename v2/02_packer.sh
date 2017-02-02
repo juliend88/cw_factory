9e7848aea594927c4d553496a79b03fa9f03447f
juliend88 <mr2plaix@gmail.com>
2017-02-02 16:31:06 +0100

    Fix

--- v2/02_packer.sh
+++ v2/02_packer.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-ls -al *
-ls outputs-glance/*
-
 NOVA_ID=$(cat outputs-glance/id.txt)
 echo $NOVA_ID
 
 
-#packer build packer_${OS_VERSION}.json
+packer build packer_${OS_VERSION}.json
