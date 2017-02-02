1490468fd27592a4ee45bf131311d6fdea36b195
juliend88 <mr2plaix@gmail.com>
2017-02-02 17:06:03 +0100

    Fix

--- v2/02_packer.sh
+++ v2/02_packer.sh
@@ -2,8 +2,5 @@
 
 NOVA_ID=$(cat outputs-glance/id.txt)
 echo $NOVA_ID
-pwd
-echo "Maxence"
-echo $(cat sources/v2/packer/packer_ubuntu.json)
 
-packer build -var "source_image=$NOVA_ID" -var 'image_name=test_packer_aaaa' -var 'factory_network=7359a091-59a7-4e29-a068-ed2a64d8c068' -var 'factory_security_group_name=start-sg-start' ./v2/packer/packer_${OS_NAME}.json
+packer build -var "source_image=$NOVA_ID" -var 'image_name=test_packer_aaaa' -var 'factory_network=7359a091-59a7-4e29-a068-ed2a64d8c068' -var 'factory_security_group_name=start-sg-start' sources/v2/packer/packer_ubuntu.json
