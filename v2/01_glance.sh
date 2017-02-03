b5292280758f433652b68c7a9c1a395ea9372a06
mohamed <lachhab.med.ali@gmail.com>
2017-02-03 14:45:33 +0100

    change openstack-cli to glance && heat ..

--- v2/01_glance.sh
+++ v2/01_glance.sh
@@ -8,9 +8,11 @@ FILE=$(echo "${IMG_URL##*/}")
 
 DATE=`date +%Y-%m-%d:%H:%M:%S`
 
-openstack image create $FILE-$DATE --disk-format qcow2 --container-format bare --file $FILE
+
+glance image-create --name $FILE-$DATE --disk-format qcow2 --container-format bare --file $FILE
 
 mkdir -p result
+
 openstack image list | grep $FILE-$DATE | awk {'print $2'} >> result/id.txt
 
 ls result/*
