24899a4be93aff8b545e13171ceab0a30270dee7
dalitun <lachhab.med.ali@gmail.com>
2017-02-06 10:41:31 +0100

    fix errors

--- v2/02_packer.sh
+++ v2/02_packer.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-IMG_TMP_ID=$(cat outputs-glance/id.txt)
+export IMG_TMP_ID=$(cat outputs-glance/id.txt)
 
 
 heat stack-create -f sources/v2/heat/template-network.yaml factory_network
-
 
 
 while true
@@ -12,10 +11,8 @@ while true
    heat stack-list | grep factory_network  | cut -d "|" -f4 | grep "CREATE_COMPLETE"
    if  [ $? -eq 0 ]
       then
-        NET_ID=$(heat output-show factory_network Network_id | sed -e 's/^"//' -e 's/"$//')
-        echo ${NET_ID}
-        SG_ID=$(heat output-show factory_network Security_group | sed -e 's/^"//'  -e 's/"$//')
-        echo ${SG_ID}
+        export NET_ID=$(heat output-show factory_network Network_id | sed -e 's/^"//' -e 's/"$//')
+        export SG_ID=$(heat output-show factory_network Security_group | sed -e 's/^"//'  -e 's/"$//')
         break
       else
         echo "Wait for factory_network stack will be up"
@@ -27,12 +24,11 @@ while true
 
 DATE=$(date +%Y-%m-%d:%H:%M:%S)
 
-IMG_NAME=${OS_NAME}-${OS_VERSION}-${DATE}
+export IMG_NAME=${OS_NAME}-${OS_VERSION}-${DATE}
 
 
 
-packer build -var 'source_image=${IMG_TMP_ID}' -var 'image_name=${IMG_NAME}'  \
-   -var 'ansible_dir=sources/v2/ansible' sources/v2/packer/packer_apt.json
+packer build sources/v2/packer/packer_apt.json
 
 
 #we delete the factory_network stack juste for testing in the we need it the test step Don't Forget :') !!!!!!!!
