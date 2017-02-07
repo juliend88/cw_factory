aaa9bbac8ad7bb35a3f7324057ddf258c6d9b75a
dalitun <lachhab.med.ali@gmail.com>
2017-02-07 17:57:31 +0100

    Rebuild

--- v2/03_update_img_cw.sh
+++ v2/03_update_img_cw.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-echo "pass"
-#IMG_ID=$(cat outputs-packer/id.txt)
 
-#PURGE=$(openstack image show -f value -c properties ${IMG_ID} | tr ", " "\n" | grep -v "^$" | cut -d"=" -f1 | grep -v -E "(cw_os|cw_origin|hw_rng_model)" | sed 's/^/--remove-property /g' | tr "\n" " ")
+IMG_ID=$(cat outputs-packer/id.txt)
 
-#glance image-update ${PURGE} ${IMG_ID}
+PURGE=$(openstack image show -f value -c properties ${IMG_ID} | tr ", " "\n" | grep -v "^$" | cut -d"=" -f1 | grep -v -E "(cw_os|cw_origin|hw_rng_model)" | sed 's/^/--remove-property /g' | tr "\n" " ")
 
-#glance image-update ${IMG_ID} --property  cw_cat=open_source --property cw_origin=Cloudwatt --property cw_os=${OS_NAME}-${OS_VERSION}
+glance image-update ${PURGE} ${IMG_ID}
 
-#glance image-update ${IMG_ID} --property  schema=/v2/schemas/image --min-disk 20
+glance image-update ${IMG_ID} --property  cw_cat=open_source --property cw_origin=Cloudwatt --property cw_os=${OS_NAME}-${OS_VERSION}
+
+glance image-update ${IMG_ID} --property  schema=/v2/schemas/image --min-disk 20
 
 
-#mkdir -p result
+mkdir -p result
 
-#echo ${IMG_ID} > result/id.txt
+echo ${IMG_ID} > result/id.txt
 
-#cat result/id.txt
+cat result/id.txt
 
-#ls result/*
+ls result/*
