4a7dfdf62ef72e6ae058d3bc6a2947aaf5882dd4
dalitun <lachhab.med.ali@gmail.com>
2017-02-08 19:31:27 +0100

    fix cloud config

--- v2/packer/provision.sh
+++ v2/packer/provision.sh
@@ -11,11 +11,11 @@ TMP=$(echo ${OS_NAME}|tr '[A-Z]' '[a-z]')
 
 if [ "${TMP}" ==  "centos" ] || [ "${TMP}" == "fedora" ]
 then
+
   if [ "${TMP}" ==  "centos" ]
   then
   sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(bc <<< "scale=0;${OS_VERSION}/1").noarch.rpm
   fi
-
 
 sudo sed -i '/^Defaults\s*requiretty$/d'/etc/sudoers
 
@@ -25,6 +25,11 @@ sudo sed -i 's|^UUID=.*|/dev/vda1|' /boot/grub/menu.lst
 
 sudo yum install -y haveged parted
 
+elif [ "${TMP}" ==  "centos" ] || [ "${TMP}" == "fedora" ]
+
+then
+   sudo apt-get update
+   sudo apt-get install -y haveged curl bzip2 unzip
 
 fi
 
