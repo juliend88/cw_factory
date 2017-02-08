fe2a94932e9155adb2c0aec0d60d78cc9938a0db
dalitun <lachhab.med.ali@gmail.com>
2017-02-08 17:37:47 +0100

    fix cloud config

--- v2/packer/provision.sh
+++ v2/packer/provision.sh
@@ -32,8 +32,8 @@ fi
 
 ### Clean
 sudo mv /tmp/cloud-config.yaml /etc/cloud/cloud.cfg
-sudo rm -rf /home/cloud/.ssh/*
-sudo service rsyslog stop
-sudo rm -rf /var/log/*
-sudo rm -rf /home/cloud/bash_history
-sudo rm -rf /var/tmp/*
+#sudo rm -rf /home/cloud/.ssh/*
+#sudo service rsyslog stop
+#sudo rm -rf /var/log/*
+#sudo rm -rf /home/cloud/bash_history
+#sudo rm -rf /var/tmp/*
