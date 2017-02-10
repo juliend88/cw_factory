94222d62d7653a09cddc55a8a05b181492a8f19b
dalitun <lachhab.med.ali@gmail.com>
2017-02-10 11:03:32 +0100

    fix cloud config

--- v2/04_test_image.sh
+++ v2/04_test_image.sh
@@ -4,11 +4,12 @@ python3 --version
 
 pip3 --version
 
-IMG_ID=$(cat outputs-for-test/id.txt)
+export NOSE_IMAGE_ID=$(cat outputs-for-test/id.txt)
 
-echo ${IMG_ID}
+cd sources/v2/pytesting_os
 
+ls
 
-ls -la sources/v2/pytesting_os
+#nosetests -sv
 
 
