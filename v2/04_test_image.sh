3b1548289f8606d8ee823e14c27e57107a7fdaac
dalitun <lachhab.med.ali@gmail.com>
2017-02-14 11:56:10 +0100

    fix os_commons.py

--- v2/04_test_image.sh
+++ v2/04_test_image.sh
@@ -1,17 +1,6 @@
 #!/bin/bash -x
 
 
-cat <<EOF > sources/v2/pytesting_os/clouds.yml
-clouds:
-  dc1:
-    auth_url: ${OS_AUTH_URL}
-    username: ${OS_USERNAME}
-    password: ${OS_PASSWORD}
-    project_name: ${OS_TENANT_NAME}
-    region_name: ${OS_REGION_NAME}
-    volume_api_version: 1
-EOF
-
 
 export NOSE_IMAGE_ID=$(cat outputs-for-test/id.txt)
 
@@ -19,7 +8,7 @@ export NOSE_FLAVOR=16
 
 export NOSE_KEYPAIR=alikey
 
-export FACTORY_NETWORK_ID=$(heat output-show factory_network Network_id | sed -e 's/^"//' -e 's/"$//')
+export NOSE_NET_ID=$(heat output-show factory_network Network_id | sed -e 's/^"//' -e 's/"$//')
 
 cd sources/v2/pytesting_os
 ./run_test.sh
