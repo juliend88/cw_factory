e7ebdf59f01e801e39b722bb7c1205ec743143d0
dalitun <lachhab.med.ali@gmail.com>
2017-02-14 11:44:28 +0100

    change python lib

--- v2/pytesting_os/os_commons.py
+++ v2/pytesting_os/os_commons.py
@@ -1,76 +1,36 @@
-import time, shade, re, paramiko
+#!/usr/bin/env python
+from openstackutils import OpenStackUtils
+import time, re, paramiko
 from os import environ as env
-import novaclient.exceptions as nova_exceptions
 
-shade.simple_logging()
-cloud = None
 
 
 def get_cloud():
-    global cloud
-    if not cloud:
-        cloud = shade.OpenStackCloud(cloud='dc1')
-    return cloud
+    return OpenStackUtils()
 
 
 def boot_vm_with_userdata_and_port(security_group, port, userdata_path):
-    server = get_cloud().create_server("test-server-" + current_time_ms(),
-                                       env['NOSE_IMAGE_ID'],
-                                       env['NOSE_FLAVOR'],
-                                       key_name=env['NOSE_KEYPAIR'],
-                                       nics={'port-id': port['id']},
-                                       security_groups=[security_group['name']],
-                                       userdata=file(userdata_path),
-                                       wait=True,
-                                       timeout=600)
-    wait_for_cloud_init(server)
+    nics = [{'port-id': port.id}]
+    server = get_cloud().nova_client.servers.create(name="vm2", image=env['NOSE_IMAGE_ID'],security_groups=[security_group['name']],
+                                                    flavor=env['NOSE_FLAVOR'], key_name=env['NOSE_KEYPAIR'], userdata=file(userdata_path), nics=nics)
 
     return server
 
-
-def wait_for_cloud_init(server):
-    counter = 0
-    while counter < 100:
-        console_log = get_console_log(server)
-        if re.search('^.*Cloud-init .* finished.*$', console_log, flags=re.MULTILINE):
-            print("Cloudinit finished in " + str(counter * 6))
-            return
-        time.sleep(6)
-        counter += 1
-    print("Cloudinit end not detected in " + str(counter * 6))
-
-
 def boot_vm(security_group, image_id=env['NOSE_IMAGE_ID'], flavor=env['NOSE_FLAVOR']):
-    server = get_cloud().create_server("test-server-" + current_time_ms(),
-                                       image_id,
-                                       flavor,
-                                       key_name=env['NOSE_KEYPAIR'],
-                                       nics={'net-id': env['FACTORY_NETWORK_ID']},
-                                       security_groups=[security_group['name']],
-                                       wait=True,
-                                       timeout=600)
-    wait_for_cloud_init(server)
+    nics = [{'net-id': env['FACTORY_NETWORK_ID']}]
+    server = get_cloud().nova_client.servers.create(name="vm2", image=image_id,security_groups=[security_group['name']],
+                                                    flavor=flavor, key_name=env['NOSE_KEYPAIR'], nics=nics)
+    print(server)
 
     return server
 
 
 def get_server(server_id):
-    try:
-        return get_cloud().get_server_by_id(server_id)
-    except nova_exceptions.NotFound:
-        return None
+    return get_cloud().nova_client.servers.get(server_id)
 
 
 def destroy_server(server):
-    get_cloud().delete_server(server['id'])
-    server = get_server(server['id'])
-    server_name = server['name']
-    counter = 0
-    while server and counter < 30:
-        time.sleep(6)
-        server = get_server(server['id'])
-        counter += 1
-    print("Destroyed server " + str(server_name) + " (detected suppression after " + str(counter * 6) + " seconds)")
+    get_cloud().nova_client.servers.delete(server)
 
 
 def current_time_ms():
@@ -78,40 +38,37 @@ def current_time_ms():
 
 
 def create_port_with_sg(security_group):
-    target_network = env['FACTORY_NETWORK_ID']
-    return get_cloud().create_port(target_network, security_groups=[security_group['id']])
+    network_id = env['FACTORY_NETWORK_ID']
+    body_value = {'port': {
+        'admin_state_up': True,
+        'security_groups': [security_group['id']],
+        'name': 'port-test'+current_time_ms(),
+        'network_id': network_id,
+    }}
+    return get_cloud().neutron_client.create_port(body=body_value)
 
 
-def get_console_log(shade_server):
-    nova_server = get_cloud().nova_client.servers.get(shade_server['id'])
-    return nova_server.get_console_output(length=200)
+
+def get_console_log(server):
+    return get_cloud().nova_client.servers.get(server.id).get_console_output(length=200)
 
 
-def get_spice_console(shade_server):
-    nova_server = get_cloud().nova_client.servers.get(shade_server['id'])
-    return nova_server.get_spice_console('spice-html5')
+
+def get_spice_console(server):
+    return get_cloud().nova_client.servers.get(server.id).get_spice_console('spice-html5')
 
 
-def create_server_snapshot(shade_server):
-    snapshot_name = 'test_snapshot-' + shade_server['name']
-    nova_server = get_cloud().nova_client.servers.get(shade_server['id'])
+def create_server_snapshot(server):
+    return get_cloud().nova_client.servers.create_image(server,server.name+current_time_ms())
 
-    snapshot_image_id = get_cloud().nova_client.servers.create_image(nova_server, snapshot_name)
 
-    snapshot_image = get_image(snapshot_image_id)
-    counter = 0
-    while snapshot_image['status'] != 'active' and counter < 30:
-        time.sleep(6)
-        snapshot_image = get_image(snapshot_image_id)
-        counter += 1
 
-    print("Created snapshot image in " + str(counter * 6) + " sec")
-
-    return snapshot_image
+def get_image(image_id):
+    return get_cloud().glance_client.images.get(image_id)
 
 
 def destroy_image(image_id):
-    get_cloud().delete_image(image_id)
+    get_cloud().glance_client.images.delete(image_id)
 
 
 def initiate_ssh(floating_ip):
@@ -135,83 +92,97 @@ def initiate_ssh(floating_ip):
     return None
 
 
-def get_image(image_id):
-    return get_cloud().get_image(image_id)
-
-
 def create_floating_ip():
-    return get_cloud().create_floating_ip(network='public')
+    floating_ip = get_cloud().nova_client.floating_ips.create('public')
+    return floating_ip
 
 
 def associate_floating_ip_to_port(floating_ip, port):
-    neutron_client = get_cloud().neutron_client
-    neutron_client.update_floatingip(floating_ip['id'], {'floatingip': {'port_id': port['id']}})
+    get_cloud().neutron_client.update_floatingip(floating_ip['id'],{'floatingip': {'port_id': port['id']}} )
 
 
 def associate_floating_ip_to_server(floating_ip, server):
-    nova_client = get_cloud().nova_client
-    nova_client.servers.get(server['id']).add_floating_ip(floating_ip['floating_ip_address'])
+    get_cloud().nova_client.servers.get(server['id']).add_floating_ip(floating_ip['floating_ip_address'])
 
 
 def create_security_group():
-    new_security_group = get_cloud().create_security_group("test-sg-" + current_time_ms(),
-                                                           "Tmp security group for testing")
-    add_ssh_ingress_rule(new_security_group['id'])
-
-    return get_cloud().get_security_group(new_security_group['id'])
+    return get_cloud().nova_client.security_groups.create(name="test"+current_time_ms(), description="Test image")
 
 
 def delete_security_group(security_group):
-    get_cloud().delete_security_group(security_group['id'])
+    get_cloud().nova_client.security_groups.delete(security_group.id)
 
 
 def delete_floating_ip(floating_ip):
-    get_cloud().delete_floating_ip(floating_ip['id'])
+    get_cloud().nova_client.floating_ips.delete(floating_ip.id)
 
 
 def delete_port(port):
-    get_cloud().delete_port(port['id'])
+    get_cloud().neutron_client.delete_port(port.id)
 
 
 def add_ssh_ingress_rule(security_group_id):
-    get_cloud().create_security_group_rule(security_group_id,
-                                           port_range_min=22,
-                                           port_range_max=22,
-                                           protocol='TCP')
-
+    get_cloud().nova_client.security_group_rules.create(security_group_id, ip_protocol="tcp",
+                                                        from_port=22, to_port=22)
 
 def create_volume():
-    created_volume = get_cloud().create_volume(5, True, 180)
-    return created_volume
+    return get_cloud().cinder_client.volumes.create(5, name="test-volume")
 
 
-def rescue(shade_server):
-    nova_client = get_cloud().nova_client
-    nova_client.servers.get(shade_server['id']).rescue()
+
+def rescue(server):
+    get_cloud().nova_client.servers.get(server.id).rescue()
 
 
-def attach_volume_to_server(shade_server, shade_volume):
-    get_cloud().attach_volume(shade_server, shade_volume, '/dev/vdb', True, 180)
-    return get_cloud().get_volume(shade_volume['id'])
+def attach_volume_to_server(server,volume):
+
+    return get_cloud().nova_client.volumes.create_server_volume(server.id, volume.id)
 
 
-def detach_volume_from_server(shade_server, shade_volume):
-    get_cloud().detach_volume(shade_server, shade_volume, True, 180)
+def detach_volume_from_server(server, volume):
+    get_cloud().nova_client.delete_server_volume(server.id,volume.id)
 
 
 def get_flavor_disk_size(flavor_id):
-    return get_cloud().get_flavor(flavor_id)
+    return get_cloud().nova_client.flavors.get_flavor(flavor_id)
 
 
-def hard_reboot(shade_server):
-    nova_client = get_cloud().nova_client
-    nova_client.servers.get(shade_server['id']).reboot(reboot_type='HARD')
+def hard_reboot(server):
+    get_cloud().nova_client.servers.get(server.id).reboot(reboot_type='HARD')
     time.sleep(60)
-    wait_for_cloud_init(shade_server)
+
 
 
 def soft_reboot(shade_server):
-    nova_client = get_cloud().nova_client
-    nova_client.servers.get(shade_server['id']).reboot(reboot_type='SOFT')
+    get_cloud().nova_client.servers.get(server.id).reboot(reboot_type='SOFT')
     time.sleep(60)
-    wait_for_cloud_init(shade_server)
+
+
+if __name__ == "__main__":
+    for i in  get_cloud().nova_client.servers.list():
+        print i
+    security_group={}
+    security_group['name']="start-sg-start"
+    security_group['id']='e273c427-71e0-4e96-af1c-cc0ae88aee1d'
+    #boot_vm(security_group)
+    #test=get_image(env['NOSE_IMAGE_ID'])
+    #print test.id
+    #server=get_server("831ae819-84e0-4bcf-bc1d-a1327b4e7c74")
+    #print server.name
+    #destroy_server(test)
+
+    #test=get_console_log(server)
+    #print test
+    #print get_spice_console(server)
+    # create_server_snapshot(server)
+    #create_floating_ip()
+    #create_security_group()
+    #get_cloud().neutron_client.delete_port("007265a6-cfa9-4db0-9af9-9e50519dcb6c")
+    #test= get_cloud().neutron_client.update_floatingip('bd25fb62-a0e1-4087-b639-567ed955a7c9',{'floatingip': {'port_id': '9c2f9f57-18c9-40f5-b9a3-4f43d41bda1a'}})
+    #create_volume()
+    #get_cloud().nova_client.volumes.delete_server_volume("831ae819-84e0-4bcf-bc1d-a1327b4e7c74","8db4ed72-52f5-4d07-8d35-af925c4bd326")
+    #print test
+    #create_port_with_sg(security_group)
+    #get_cloud().neutron_client.delete_port("605b7e4f-abde-4df0-9b58-abe532908a46")
+    #keypair = get_cloud().nova_client.keypairs.create("testvm",public_key=file(test))
+    #print(keypair)
