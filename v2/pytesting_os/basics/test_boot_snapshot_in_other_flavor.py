e2086dd96c36a41ded5c114d166471315f90f3f1
dalitun <lachhab.med.ali@gmail.com>
2017-02-21 18:29:05 +0100

    fix tests

--- v2/pytesting_os/basics/test_boot_snapshot_in_other_flavor.py
+++ v2/pytesting_os/basics/test_boot_snapshot_in_other_flavor.py
@@ -1,20 +1,34 @@
-import os_commons as cwlib
+import time
+from basics import test_resources
+import openstackutils
 from basics import test_resources
 
+
+cwlib = openstackutils.OpenStackUtils()
 
 def test_boot_snapshot_in_other_flavor():
     global test_resources
 
     snapshot_image = cwlib.create_server_snapshot(test_resources['my_server'])
 
-    new_server = cwlib.boot_vm(test_resources['my_sg'], snapshot_image['id'], 17)
-    floating = cwlib.create_floating_ip(test_resources['my_floating'])
+    print "the id of snapshot is:"+snapshot_image
+
+    cwlib.wait_server_available(test_resources['my_server'])
+
+    new_server = cwlib.boot_vm(image_id=snapshot_image, flavor=21,keypair=test_resources['my_keypair'])
+
+    floating = cwlib.create_floating_ip()
     cwlib.associate_floating_ip_to_server(floating, new_server)
-
+    time.sleep(20)
     ssh_connection = cwlib.initiate_ssh(floating)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh_connection.exec_command('sudo ls /home')
+    ssh_test = ssh_stdout.read()
+    test_boot_snapshot=(ssh_test.find('cloud') != -1)
 
-    assert ssh_connection
+    assert test_boot_snapshot
 
     cwlib.destroy_server(new_server)
 
-    cwlib.destroy_image(snapshot_image['id'])
+    cwlib.destroy_image(snapshot_image)
+
+    cwlib.delete_floating_ip(floating)
