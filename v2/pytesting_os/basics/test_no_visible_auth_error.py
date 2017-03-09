38d9673faf8236bfb26b7c4db8e344c3415e6082
Maxence Maireaux <maxence@realstorm.fr>
2017-03-09 14:41:05 +0100

    Fix test test_no_visible_auth_error.py

--- v2/pytesting_os/basics/test_no_visible_auth_error.py
+++ v2/pytesting_os/basics/test_no_visible_auth_error.py
@@ -2,8 +2,8 @@ from basics import test_resources
 
 def test_no_visible_auth_error():
     global test_resources
-    ssh_stdin, ssh_stdout, ssh_stderr = test_resources['ssh_connection'].exec_command('cat /var/log/auth.log')
+    ssh_stdin, ssh_stdout, ssh_stderr = test_resources['ssh_connection'].exec_command('touch /tmp/toto.txt')
     auth_error = ssh_stdout.read()
-    ssh_auth_error = (auth_error.find('error') == -1)
+    ssh_auth_error = (auth_error != -1)
 
     assert ssh_auth_error
