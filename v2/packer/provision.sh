b47086ea5899347b73df13df3b7b6058a68b3b0a
Maxence Maireaux <maxence@realstorm.fr>
2017-03-20 16:21:38 +0100

    Fix fedora 25

--- v2/packer/provision.sh
+++ v2/packer/provision.sh
@@ -12,7 +12,7 @@ if [ "${TMP}" ==  "centos" ] || [ "${TMP}" == "fedora" ]
        sudo yum install -y haveged parted curl unzip wget
       else
        sudo dnf install -y haveged parted curl unzip wget
-       sudo echo root | passwd --stdin root
+       sudo passwd --stdin root < root
     fi
 
     if [ "$(echo ${VER})" == "6" ]
