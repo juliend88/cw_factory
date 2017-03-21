51ad461e5654564a0db45858d7df8f1ae9cefb34
Maxence Maireaux <maxence@realstorm.fr>
2017-03-21 10:21:02 +0100

    Update property & OS_LOGO

--- v2/install-pipeline.sh
+++ v2/install-pipeline.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 ## Ubuntu
-fly set-pipeline --target factory --config base-image-factory.yml --pipeline ubuntu-14.04 --load-vars-from credentials.yml -v OS_URL=http://cloud-images.ubuntu.com/releases/14.04/release/ubuntu-14.04-server-cloudimg-amd64-disk1.img -v OS_NAME=ubuntu -v OS_VERSION=14.04
-fly set-pipeline --target factory --config base-image-factory.yml --pipeline ubuntu-16.04 --load-vars-from credentials.yml -v OS_URL=http://cloud-images.ubuntu.com/releases/16.04/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img -v OS_NAME=ubuntu -v OS_VERSION=16.04
+fly set-pipeline --target factory --config base-image-factory.yml --pipeline ubuntu-14.04 --load-vars-from credentials.yml -v OS_URL=http://cloud-images.ubuntu.com/releases/14.04/release/ubuntu-14.04-server-cloudimg-amd64-disk1.img -v OS_NAME=ubuntu -v OS_VERSION=14.04 -v OS_LOGO=lin-ubuntu.png
+fly set-pipeline --target factory --config base-image-factory.yml --pipeline ubuntu-16.04 --load-vars-from credentials.yml -v OS_URL=http://cloud-images.ubuntu.com/releases/16.04/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img -v OS_NAME=ubuntu -v OS_VERSION=16.04 -v OS_LOGO=lin-ubuntu.png
 
 ## Debian
-fly set-pipeline --target factory --config base-image-factory.yml --pipeline debian-8 --load-vars-from credentials.yml -v OS_URL=http://cdimage.debian.org/cdimage/openstack/current-8/debian-8-openstack-amd64.qcow2 -v OS_NAME=debian -v OS_VERSION=8
+fly set-pipeline --target factory --config base-image-factory.yml --pipeline debian-8 --load-vars-from credentials.yml -v OS_URL=http://cdimage.debian.org/cdimage/openstack/current-8/debian-8-openstack-amd64.qcow2 -v OS_NAME=debian -v OS_VERSION=8 -v OS_LOGO=lin-debian.png
 
 ## Fedora
-fly set-pipeline --target factory --config base-image-factory.yml --pipeline fedora-25 --load-vars-from credentials.yml -v OS_URL=https://storage.fr1.cloudwatt.com/v1/AUTH_6362790faac34793a4506a83bf353ef3/iso%3A/Fedora-Cloud-Base-25-1.3.x86_64.raw -v OS_NAME=fedora -v OS_VERSION=25
+fly set-pipeline --target factory --config base-image-factory.yml --pipeline fedora-25 --load-vars-from credentials.yml -v OS_URL=https://storage.fr1.cloudwatt.com/v1/AUTH_6362790faac34793a4506a83bf353ef3/iso%3A/Fedora-Cloud-Base-25-1.3.x86_64.raw -v OS_NAME=fedora -v OS_VERSION=25 -v OS_LOGO=lin-fedora.png
 
 ## CentOS
-fly set-pipeline --target factory --config base-image-factory.yml --pipeline centos-6 --load-vars-from credentials.yml -v OS_URL=http://cloud.centos.org/centos/6/images/CentOS-6-x86_64-GenericCloud.qcow2 -v OS_NAME=centos -v OS_VERSION=6
-fly set-pipeline --target factory --config base-image-factory.yml --pipeline centos-7 --load-vars-from credentials.yml -v OS_URL=http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2 -v OS_NAME=centos -v OS_VERSION=7
+fly set-pipeline --target factory --config base-image-factory.yml --pipeline centos-6 --load-vars-from credentials.yml -v OS_URL=http://cloud.centos.org/centos/6/images/CentOS-6-x86_64-GenericCloud.qcow2 -v OS_NAME=centos -v OS_VERSION=6 -v OS_LOGO=lin-centos.png
+fly set-pipeline --target factory --config base-image-factory.yml --pipeline centos-7 --load-vars-from credentials.yml -v OS_URL=http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2 -v OS_NAME=centos -v OS_VERSION=7 -v OS_LOGO=lin-centos.png
